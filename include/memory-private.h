5dc8133d638f66bcf89acd0f3bb46c33ae468450
Telmo <telmoalexandre2505@gmail.com>
2021-10-23 15:25:09 +0100

    teste

--- include/memory-private.h
+++ include/memory-private.h
@@ -1,21 +1,21 @@
 #ifndef MEMORY_PRIVATE_H_GUARD
 #define MEMORY_PRIVATE_H_GUARD
-#define MAX 50
+#define MAX_STRINGS 50
+#define MAX_LENGHT 258
+
 
 
 //estrutura que representa um buffer circular, completar pelos alunos
-struct circular_buffer { 	
-    char tamanho[MAX][MAX];
-    int posicaoEscrever;
-    int posicaoLer;
+struct circular_buffer { 
+    
 
 };
 
 //estrutura que representa um buffer de acesso aleatório, completar pelos alunos
 struct rnd_access_buffer { 	
-    
+    char *buffer[MAX_STRINGS][MAX_LENGHT];
     int enderecoArray;
-    int posicaoBuffer[MAX];
+    int posicaoBuffer[MAX_STRINGS];
     
 };
 
